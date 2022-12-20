83e32c25ee433c1539bf3d8f0fcaab5aec07c46a
Akash praveen kumar R <rakashpraveen@karunya.edu.in>
2022-12-21 02:08:21 +0530

    commit

---
+++ src/components/Students.js
@@ -0,0 +1,19 @@
+const Students = [
+  {
+    id: "1",
+    Name: "Akash",
+    Age: 25,
+  },
+  {
+    id: "2",
+    Name: "Praveen",
+    Age: 25,
+  },
+  {
+    id: "3",
+    Name: "kumar",
+    Age: 25,
+  },
+];
+
+export default Students;
